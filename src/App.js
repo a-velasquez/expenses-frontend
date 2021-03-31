711473af904cd8dfae620e8168afd6cbabd8250a
a-velasquez <avelasquez075@gmail.com>
2021-03-30 18:16:37 -0700

    fix error in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux'
-
 
 class App extends React.Component {
-
   render() {
-    return <div className="App">Accounts Component</div>;
+    return (
+      <div className="App">
+        Accounts Component
+      </div>
+    );
   }
 }
 
 export default App;
+
