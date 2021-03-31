620fc5b9f328d067673f0ea77c92a4c2db85cbd9
a-velasquez <avelasquez075@gmail.com>
2021-03-31 12:10:29 -0700

    import accountReducer and finish store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from 'react-redux';
+import accountReducer from './reducers/accountReducer'
 
 
 // set up store
@@ -11,7 +12,10 @@ import App from "./App";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+let store = createStore(
+  accountReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
