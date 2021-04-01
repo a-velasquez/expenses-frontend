2403e246c1bff341c58352fe5a279ab86cb5e576
a-velasquez <avelasquez075@gmail.com>
2021-03-31 23:33:02 -0700

    add AccountInput component

---
+++ src/components/AccountInput.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class AccountInput extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Account Input Goes Here
+      </div>
+    )
+  }
+}
+
+export default AccountInput;
