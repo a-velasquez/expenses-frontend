3243909a2c946e0d3060a46d600ba8afc460aeb1
a-velasquez <avelasquez075@gmail.com>
2021-03-31 23:21:42 -0700

    import AccountsContainer.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux"
 import { fetchAccounts } from "./actions/fetchAccounts"
-
+import AccountsContainer from "./containers/AccountsContainer"
 class App extends React.Component {
 
   componentDidMount() {
@@ -12,7 +12,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        Accounts Component
+        <AccountsContainer />
       </div>
     );
   }
