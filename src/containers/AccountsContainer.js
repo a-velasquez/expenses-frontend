f02fc7d3d71e485203bea3ba27a2a8c71e552004
a-velasquez <avelasquez075@gmail.com>
2021-03-31 23:21:26 -0700

    add AccountsContainer.js

---
+++ src/containers/AccountsContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class AccountsContainer extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+        Accounts Container
+      </div>
+    )
+  }
+}
+
+export default AccountsContainer;
