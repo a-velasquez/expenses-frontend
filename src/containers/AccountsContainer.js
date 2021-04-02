fb06826723cef01ede2897ad8fd1966e25a71159
a-velasquez <avelasquez075@gmail.com>
2021-04-02 14:44:31 -0700

    import Accounts and add to AccountsContainer

--- src/containers/AccountsContainer.js
+++ src/containers/AccountsContainer.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AccountInput from "../components/AccountInput";
+import Accounts from "../components/Accounts"
 class AccountsContainer extends React.Component {
 
 
   render() {
     return (
       <div>
-        Accounts Container
+        <AccountInput />
+        <Accounts/>
       </div>
     )
   }
