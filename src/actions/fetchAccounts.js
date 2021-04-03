0c81c6548becbfd1fd6408b0f9059b70b05507c1
a-velasquez <avelasquez075@gmail.com>
2021-04-02 22:45:26 -0700

    finish fetchAccounts

--- src/actions/fetchAccounts.js
+++ src/actions/fetchAccounts.js
@@ -1,3 +1,11 @@
-export function fetchAccounts(action) {
-  return action
+export function fetchAccounts() {
+  console.log('inside fetch accounts function')
+  return (dispatch) => {
+    fetch("http://localhost:3000/api/v1/accounts")
+      .then(resp => resp.json())
+      .then(accounts => dispatch({
+        type: 'FETCH_ACCOUNTS',
+        payload: accounts
+      }));
+  }
 }
