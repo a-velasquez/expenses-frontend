dd1a5211be2f75e02b4f2ea0dbb5e71c85382401
a-velasquez <avelasquez075@gmail.com>
2021-04-08 14:14:08 -0700

    add handleChange function and make form controlled

--- src/components/AccountInput.js
+++ src/components/AccountInput.js
@@ -2,16 +2,25 @@ import React from "react";
 class AccountInput extends React.Component {
   
   // local state
-  state = {name: '', balance: ''}
+  state = {
+    name: '',
+    balance: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
 
   render() {
     return (
       <div>
         <form>
           <label>Checking Account Name:</label>
-          <input type="text" placeholder="Name" value={this.state.name}/><br/>
+          <input type="text" placeholder="Name" name="name" onChange={this.handleChange} value={this.state.name}/><br/>
           <label>Checking Account Balance:</label>
-          <input type="text" placeholder="Balance" value={this.state.balance}/>
+          <input type="text" placeholder="Balance" name="balance" onChange={this.handleChange} value={this.state.balance}/>
         </form>
       </div>
     )
