a448d88413861f04c5c0197598b09b41070a8da0
a-velasquez <avelasquez075@gmail.com>
2021-04-08 14:00:36 -0700

    added value tags to form inputs

--- src/components/AccountInput.js
+++ src/components/AccountInput.js
@@ -1,14 +1,17 @@
 import React from "react";
 class AccountInput extends React.Component {
+  
+  // local state
+  state = {name: '', balance: ''}
 
   render() {
     return (
       <div>
         <form>
           <label>Checking Account Name:</label>
-          <input type="text" placeholder="Name"/><br/>
+          <input type="text" placeholder="Name" value={this.state.name}/><br/>
           <label>Checking Account Balance:</label>
-          <input type="text" placeholder="Balance"/>
+          <input type="text" placeholder="Balance" value={this.state.balance}/>
         </form>
       </div>
     )
