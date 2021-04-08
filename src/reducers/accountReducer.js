f1180e04b01af4171e4added4f51e5935801df69
a-velasquez <avelasquez075@gmail.com>
2021-04-08 13:21:08 -0700

    add debugger and notes

--- src/reducers/accountReducer.js
+++ src/reducers/accountReducer.js
@@ -1,7 +1,8 @@
 export default function accountReducer(state = { accounts: [] }, action) {
+  // debugger;
   switch (action.type) {
     case 'FETCH_ACCOUNTS':
-      return { accounts: action.payload }
+      return { accounts: action.payload } //returns array from fetch request instead of empty array
     default:
       return state 
   }
