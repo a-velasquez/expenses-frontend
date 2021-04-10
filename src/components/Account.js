1617c38d850c3d170f3d6d92da030d32a5f4efeb
a-velasquez <avelasquez075@gmail.com>
2021-04-09 17:40:49 -0700

    Account returns list item of each account's name and balance

--- src/components/Account.js
+++ src/components/Account.js
@@ -4,6 +4,11 @@ import React from 'react';
 
 const Account = (props) => {
   
+  return (
+    <li>
+       {props.account.name} - {props.account.balance}
+    </li>
+  )
 }
 
 export default Account;
