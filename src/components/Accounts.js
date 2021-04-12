1f05f79499519de1f38d7ee9b3d66dc220c1ac73
a-velasquez <avelasquez075@gmail.com>
2021-04-12 16:11:49 -0700

    add link to account name and balance

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -1,12 +1,19 @@
 import React from "react";
-import Account from './Account'
+import { Route, Link } from "react-router-dom";
+import Account from "./Account";
 
 const Accounts = (props) => {
   return (
     <div>
-      {props.accounts.map(account => <div key={account.id}><Account account={ account }></Account></div>)}
+      {props.accounts.map((account) => (
+        <li key={account.id}>
+          <Link to={`/accounts/${account.id}`}>
+            {account.name} - ${account.balance}
+          </Link>
+        </li>
+      ))}
     </div>
-  )
-} 
+  );
+};
 
 export default Accounts;
