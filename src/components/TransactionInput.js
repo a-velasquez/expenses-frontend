aa143736478462baf119038155d75a4b80cf5f2c
a-velasquez <avelasquez075@gmail.com>
2021-04-12 18:03:43 -0700

    add TransactionInput component

---
+++ src/components/TransactionInput.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import TransactionsContainer from '../containers/TransactionsContainer';
+
+class TransactionInput extends React.Component {
+
+}
+
+export default TransactionInput;
