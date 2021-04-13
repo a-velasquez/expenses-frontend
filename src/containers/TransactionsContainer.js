b2cf01739f98adb2accc74b406f91435c087084c
a-velasquez <avelasquez075@gmail.com>
2021-04-12 17:30:48 -0700

    create TransactionsContainer

---
+++ src/containers/TransactionsContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class TransactionsContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        Transactions container
+      </div>
+    )
+  }
+}
+
+export default TransactionsContainer;
