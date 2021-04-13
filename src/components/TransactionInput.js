58fb2493857f1587f4d2ce37136dd69a6263bf25
a-velasquez <avelasquez075@gmail.com>
2021-04-12 18:15:15 -0700

    import and render Transactions component

--- src/components/TransactionInput.js
+++ src/components/TransactionInput.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import TransactionsContainer from '../containers/TransactionsContainer';
+import Transactions from '../components/Transactions';
 
 class TransactionInput extends React.Component {
-
+  render() {
+    return (
+      <div>
+        Transaction Input
+         <Transactions />
+      </div>
+    )
+  }
 }
 
 export default TransactionInput;
