aa22098fcb717a9605d6a554512abadadedfd7c4
a-velasquez <avelasquez075@gmail.com>
2021-04-13 22:33:01 -0700

    pass props.transactions and return list of transactions

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 const Transactions = (props) => {
+
   return (
     <div>
-      Transactions
+      {/* checks if transactions exist first then maps them instead of returning undefined props */}
+      {props.transactions && props.transactions.map(
+        transaction => <li key={transaction.id}>
+          {transaction.kind} - {transaction.amount} posted - {transaction.date}
+        </li>
+      )}
     </div>
   )
 }
