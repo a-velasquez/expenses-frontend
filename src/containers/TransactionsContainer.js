751e610f919d691cac5f0aea1bae9af137f12166
a-velasquez <avelasquez075@gmail.com>
2021-04-13 21:14:20 -0700

    import Transactions component and pass account props

--- src/containers/TransactionsContainer.js
+++ src/containers/TransactionsContainer.js
@@ -1,13 +1,16 @@
 import React from "react";
 import TransactionInput from "../components/TransactionInput";
-
+import Transactions from "../components/Transactions";
 class TransactionsContainer extends React.Component {
   render() {
     return (
       <div>
-        <TransactionInput/>
+        <TransactionInput />
+        <Transactions
+          transactions={this.props.account && this.props.account.transactions}
+        />
       </div>
-    )
+    );
   }
 }
 
