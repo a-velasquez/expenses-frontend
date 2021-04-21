fcc2aa4c4664f0cf34b75401d55222da592c2f36
a-velasquez <avelasquez075@gmail.com>
2021-04-21 16:51:55 -0700

    add submit function and set local state default

--- src/components/TransactionInput.js
+++ src/components/TransactionInput.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
-
+import { addTransaction } from "../actions/addTransaction";
 class TransactionInput extends React.Component {
-  
   //local state
   state = {
-    kind: "",
+    kind: "deposit",
     amount: "",
   };
 
@@ -15,12 +13,20 @@ class TransactionInput extends React.Component {
       [event.target.name]: event.target.value,
     });
   };
-  
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.addTransaction(this.state, this.props.account.id);
+    this.setState({
+      kind: "deposit",
+      amount: "",
+    });
+  };
 
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>Transaction Type:</label>
           <select
             name="kind"
@@ -30,9 +36,13 @@ class TransactionInput extends React.Component {
             <option>deposit</option>
             <option>withdraw</option>
           </select>
-          <br />
           <label>Transaction Amount:</label>
-          <input type="text" name="amount" value={this.state.amount} onChange={ this.handleChange}/>
+          <input
+            type="text"
+            name="amount"
+            value={this.state.amount}
+            onChange={this.handleChange}
+          />
           <input type="submit" />
         </form>
       </div>
@@ -40,4 +50,4 @@ class TransactionInput extends React.Component {
   }
 }
 
-export default TransactionInput;
+export default connect(null, { addTransaction })(TransactionInput);
